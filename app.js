8268a71fb9744994a8d2c2d3f9a14b0d8b205b2a
Ben Foxall <benfoxall@gmail.com>
2011-12-03 12:40:28 +0000

    added basic /data route which returns some json

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , api = require('./lib/api.js')
+  ;
 
 var app = module.exports = express.createServer();
 
@@ -30,6 +32,23 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+
+
+
+app.get('/data', function(req,res){
+  api.Request({result:'geo', pagesize:50})
+  .on('success', function(data){
+    var activities = data['iati-activity']
+    
+    //just send it straight to the client
+    res.send(activities);
+    
+  }).end()
+  
+  
+  
+});
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
