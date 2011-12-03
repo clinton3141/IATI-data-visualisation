acf7b1a9b6ec6990828401e23394806866e813fe
Clinton Montague <clinton@slightlymore.co.uk>
2011-12-03 13:44:35 +0000

    Fixed typo - process.env.PORT instead of ps.env.PORT

--- app.js
+++ app.js
@@ -45,5 +45,5 @@ app.get('/data', function(req,res){
 });
 
 
-app.listen(ps.env.PORT|| 3000);
+app.listen(process.env.PORT|| 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
