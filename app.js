71988f473b0aafd1e3504fc8948b1387a23ee8a0
Clinton Montague <clinton@slightlymore.co.uk>
2011-12-03 15:51:26 +0000

    simple country query

--- app.js
+++ app.js
@@ -37,17 +37,27 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+app.get ('/country-detail/:id', function (req, res) {
+	api.Request({result:'geo', pagesize:50, country:req.params.id})
+		.on('success', function (data) {
+			var activities = data['iata-activity'];
 
-app.get('/data', function(req,res){
-  api.Request({result:'geo', pagesize:50})
-  .on('success', function(data){
-    var activities = data['iati-activity'];
-
-    //just send it straight to the client
-    res.send(activities);
-
-  }).end();
+			res.send(data);
+		}).end();
 });
+
+// app.get('/data/*', function(req,res){
+	// console.log (req);
+	// res.send(req);
+  // api.Request({result:'geo', pagesize:50})
+  // .on('success', function(data){
+  //   var activities = data['iati-activity'];
+
+  //   //just send it straight to the client
+  //   res.send(data);
+
+  // }).end();
+// });
 
 io.sockets.on('connection', function (socket) {
 	console.log('connect!');
